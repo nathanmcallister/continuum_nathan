6a54ddac3de400aa9777ac74f6a3ec5c22f5916f
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2023-12-17 05:34:23 -0700

    Created NARX model on SMD example

--- simulation/narx_test.py
+++ simulation/narx_test.py
@@ -6,22 +6,20 @@ from spring_mass_damper import *
 from typing import List, Tuple
 import numpy as np
 
-
-device = (
-    "cuda" 
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-
-print(f"Using {device} device")
-
 class NARX(nn.Module):
     def __init__(self, state_dim: int, action_dim: int, hidden_layers: List[int], activation=nn.ReLU(), output_activation=None, num_previous_states: int = 1, num_previous_actions: int = 0):
         super().__init__()
 
         self.flatten = nn.Flatten()
+
+        self.device = (
+            "cuda" 
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+        print(f"Using {self.device} device")
 
         self.model = nn.Sequential(OrderedDict([
             ('input', nn.Linear((1+num_previous_states) * state_dim + (1+num_previous_actions) * action_dim, hidden_layers[0]).double()),
@@ -37,13 +35,54 @@ class NARX(nn.Module):
         if output_activation:
             self.model.add_module('output_activation', output_activation)
 
-    def forward(self, x: Tuple[List[np.ndarray], ...]):
-        states = np.concatenate([state.flatten() for state in x[0]])
-        actions = np.concatenate([action.flatten() for action in x[1]])
-        input = torch.from_numpy(np.concatenate([states, actions]))
-        logits = self.model(input)
-        return logits
+        self.model = self.model.to(self.device)
+
+        self.loss = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+    
+    def _form_input_tensor(self, input: Tuple[List[np.ndarray], ...]) -> torch.tensor:
+        observations, actions = input[0], input[1]
+        obs = np.concatenate([obs.flatten() for obs in observations])
+        act = np.concatenate([act.flatten() for act in actions])
+        return torch.from_numpy(np.concatenate([obs, act]))
+
+    def forward(self, x):
+        return self.model(x)
+
+    def train(self, data_batch: List[Tuple[Tuple[List[np.ndarray], ...], ...]], display_training_data: bool = False) -> None:
+        X, y = [], []
+
+        for input, output in data_batch:
+            y.append(torch.from_numpy(output))
+            X.append(self._form_input_tensor(input))
+
+        X, y = torch.stack(X), torch.stack(y)
+
+        self.model.train()
+
+        X, y = X.to(self.device), y.to(self.device)
+
+        pred = self.model(X)
+        loss = self.loss(pred, y)
+
+        if display_training_data:
+            print("Loss:", loss)
+
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
 
 
-bingo = NARX(2, 1, [12, 14])
-print(bingo(([np.array([[0.0],[1.0]]), np.array([[0.1],[0.9]])], [np.array([1.0])])))
+def train_model(narx_model: NARX, system_model: SMD, model_save_location: str, batch_size: int = 64, num_epochs: int = 2**12, display_epochs: int = 128):
+    for epoch in range(num_epochs):
+        data_batch = system_model.get_data_batch(batch_size)
+
+        if epoch % display_epochs == 0:
+            narx_model.train(data_batch, True)
+        else:
+            narx_model.train(data_batch)
+
+bingo = NARX(1, 1, [16, 16])
+bongo = SMD(1, 0.5, 1, .1)
+
+train_model(bingo, bongo, "boop")
--- simulation/spring_mass_damper.py
+++ simulation/spring_mass_damper.py
@@ -1,9 +1,9 @@
 import numpy as np
 from scipy.linalg import expm
-from typing import Tuple
+from typing import Tuple, List
 from math import factorial
 
-def dlsim(A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, u: np.ndarray, x0: np.ndarray = None) -> np.ndarray:
+def dlsim(A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, u: np.ndarray, x0: np.ndarray) -> np.ndarray:
     
     assert A.shape[0] == A.shape[1]
 
@@ -21,14 +21,12 @@ def dlsim(A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, u: np.ndar
 
     m = C.shape[0]
 
-    if x0 != None:
-        assert x0.shape == (n,1)
+    assert x0.shape == (n,1)
 
     t = u.shape[1]
 
     x = np.zeros((n, t+1), dtype=float)
-    if x0 != None:
-        x[:,0] = x0.flatten()
+    x[:,0] = x0.flatten()
 
     y = np.zeros((m, t+1), dtype=float)
 
@@ -58,32 +56,58 @@ def c2d(Ac: np.ndarray, Bc: np.ndarray, dt: float) -> Tuple[np.ndarray, ...]:
 
     return A, B
 
+class SMD:
 
-def spring_mass_damper(k: float, b: float, m: float, dt: float, output_type: str = "position") -> Tuple[np.ndarray, ...]:
-    valid_output_types = ["position", "velocity", "acceleration"]
+    def __init__(self, k: float, b: float, m: float, dt: float, output_type: str = "position"):
+        valid_output_types = ["position", "velocity", "acceleration"]
+        assert output_type in valid_output_types
 
-    assert output_type in valid_output_types
+        assert k >= 0 and b >= 0 and m > 0
+        
+        self.dt = dt
+        self.output_type = output_type
 
-    assert k >= 0 and b >= 0 and m > 0
+        Ac = np.array([[0, 1], [-k/m, -b/m]], dtype=float)
+        Bc = np.array([[0],[1/m]], dtype=float)
 
-    Ac = np.array([[0, 1], [-k/m, -b/m]], dtype=float)
-    Bc = np.array([[0],[1/m]], dtype=float)
+        if output_type == valid_output_types[0]: # Position
+            self.C = np.array([1,0], dtype=float).reshape((1,2))
+            self.D = np.zeros((1,1), dtype=float)
 
-    if output_type == valid_output_types[0]: # Position
-        C = np.array([1,0], dtype=float).reshape((1,2))
-        D = np.zeros((1,1), dtype=float)
+        elif output_type == valid_output_types[1]: # Velocity
+            self.C = np.array([0,1], dtype=float).reshape((1,2))
+            self.D = np.zeros((1,1), dtype=float)
 
-    elif output_type == valid_output_types[1]: # Velocity
-        C = np.array([0,1], dtype=float).reshape((1,2))
-        D = np.zeros((1,1), dtype=float)
+        elif output_type == valid_output_types[2]: # Acceleration
+            self.C = np.array([-k/m,-b/m], dtype=float).reshape((1,2))
+            self.D = np.ones((1,1), dtype=float) / m
+        else:
+            print("wat")
 
-    elif output_type == valid_output_types[2]: # Acceleration
-        C = np.array([-k/m,-b/m], dtype=float).reshape((1,2))
-        D = np.ones((1,1), dtype=float) / m
-    else:
-        print("wat")
+        self.A, self.B = c2d(Ac, Bc, dt) 
+    
+    def get_data_batch(self, batch_size: int, num_previous_obs: int = 1, num_previous_acts: int = 0) -> List[Tuple[Tuple[List[np.ndarray], ...], ...]]:
+        assert batch_size > 0 and num_previous_obs >= 0 and num_previous_acts >= 0
 
-    A, B = c2d(Ac, Bc, dt) 
+        num_timesteps = batch_size + max(num_previous_obs, num_previous_acts) + 1
 
-    return A, B, C, D
+        x0 = np.random.randn(2,1)
+        u_sim = np.random.randn(1,num_timesteps-1)
+
+        y_sim = dlsim(self.A, self.B, self.C, self.D, u_sim, x0)
+        batch = []
+        if num_previous_obs >= num_previous_acts:
+            for i in range(num_previous_obs, num_timesteps - 1):
+                X, u = [], []
+                for j in range(num_previous_obs+1):
+                    X.append(y_sim[:, i - num_previous_obs + j])
+                for j in range(num_previous_acts+1):
+                    u.append(u_sim[:, i - num_previous_acts + j])
+
+                input_tuple = (X, u)
+                y = y_sim[:, i+1]
+
+                batch.append((input_tuple, y))
+
+        return batch
 
