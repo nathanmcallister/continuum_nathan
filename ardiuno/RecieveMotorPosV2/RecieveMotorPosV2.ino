7f21d4e65cc78d4ece88a3eaa7c22dd8930fbb54
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2024-01-15 08:30:07 -0500

    Update RecieveMotorPosV2.ino to fix servo frequency

--- ardiuno/RecieveMotorPosV2/RecieveMotorPosV2.ino
+++ ardiuno/RecieveMotorPosV2/RecieveMotorPosV2.ino
@@ -39,7 +39,23 @@ void setup() {
     
     // Start PWM driver
     pwm.begin();
-    pwm.setOscillatorFrequency(27000000);
+    /*
+    * In theory the internal oscillator (clock) is 25MHz but it really isn't
+    * that precise. You can 'calibrate' this by tweaking this number until
+    * you get the PWM update frequency you're expecting!
+    * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+    * is used for calculating things like writeMicroseconds()
+    * Analog servos run at ~50 Hz updates, It is importaint to use an
+    * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+    * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+    *    the I2C PCA9685 chip you are setting the value for.
+    * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+    *    expected value (50Hz for most ESCs)
+    * Setting the value here is specific to each individual I2C PCA9685 chip and
+    * affects the calculations for the PWM update frequency. 
+    * Failure to correctly set the int.osc value will cause unexpected PWM results
+    */
+    pwm.setOscillatorFrequency(25100000);
     pwm.setPWMFreq(SERVO_FREQ);
 
     delay(10);
