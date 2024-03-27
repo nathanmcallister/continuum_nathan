2a3359ffef78648ed24b1778f340f7bd0a601e41
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2024-03-27 12:57:56 -0400

    Making registration code more readable

--- matlab/rigid_registration.m
+++ matlab/rigid_registration.m
@@ -22,8 +22,8 @@ T_AURORA_2_MODEL_FILE = "../tools/T_aurora_2_model";
 T_TIP_2_COIL_FILE = "../tools/T_tip_2_coil";
 
 %% File inputs
-model_reg_pos_in_sw = readmatrix(SW_MODEL_POS_FILE);
-tip_reg_pos_in_sw = readmatrix(SW_TIP_POS_FILE);
+model_reg_truth_in_sw = readmatrix(SW_MODEL_POS_FILE);
+tip_reg_truth_in_sw = readmatrix(SW_TIP_POS_FILE);
 
 T_sw_2_model = readmatrix(T_SW_2_MODEL_FILE);
 T_sw_2_tip = readmatrix(T_SW_2_TIP_FILE);
@@ -32,8 +32,8 @@ penprobe = readmatrix(PEN_FILE);
 reg_table = readtable(REG_FILE);
 
 %% Get truth registration positions
-model_reg_pos_in_model = T_mult(T_sw_2_model, model_reg_pos_in_sw);
-tip_reg_pos_in_tip = T_mult(T_sw_2_tip, tip_reg_pos_in_sw);
+model_reg_truth_in_model = T_mult(T_sw_2_model, model_reg_truth_in_sw);
+tip_reg_truth_in_tip = T_mult(T_sw_2_tip, tip_reg_truth_in_sw);
 
 %% Extracting and processing aurora data
 pen_mat = table2array(reg_table(strcmp('0A', reg_table{:, 3}), 4:10));
@@ -46,13 +46,13 @@ for i=1:size(pen_mat, 1)
 end
 
 % Split into model registration points and tip registration points
-model_reg_meas_in_aurora = pen_positions(:, 1:size(model_reg_pos_in_model, 2));
-tip_reg_meas_in_aurora = pen_positions(:, size(model_reg_pos_in_model, 2)+1:end);
+model_reg_meas_in_aurora = pen_positions(:, 1:size(model_reg_truth_in_model, 2));
+tip_reg_meas_in_aurora = pen_positions(:, size(model_reg_truth_in_model, 2)+1:end);
 
 %% SVD rigid registrations
-[~, T_aurora_2_model, rmse_aurora_2_model] = rigid_align_svd(model_reg_meas_in_aurora, model_reg_pos_in_model)
+[~, T_aurora_2_model, rmse_aurora_2_model] = rigid_align_svd(model_reg_meas_in_aurora, model_reg_truth_in_model)
 
-[~, T_aurora_2_tip, rmse_aurora_2_tip] = rigid_align_svd(tip_reg_meas_in_aurora, tip_reg_pos_in_tip)
+[~, T_aurora_2_tip, rmse_aurora_2_tip] = rigid_align_svd(tip_reg_meas_in_aurora, tip_reg_truth_in_tip)
 
 %% Coil to Aurora registration
 mean_tip_quat = quat_mean(tip_mat(:,1:4));
@@ -60,7 +60,6 @@ mean_tip_pos = mean(tip_mat(:, 5:end));
 
 T_coil_2_aurora = [[quat2dcm(mean_tip_quat), mean_tip_pos']; [0 0 0 1]]
 rmse_coil_2_aurora = sqrt(mean((tip_mat(:,5:end) - mean_tip_pos).^2, 'all'))
-
 
 %% Final transforms
 T_tip_2_coil = T_coil_2_aurora^-1 * T_aurora_2_tip^-1
