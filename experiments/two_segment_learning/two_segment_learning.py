c6e005c8aea1ce8eaec92d4764147145ff0bae7c
Camty1 <cameron.j.wolfe.th@dartmouth.edu>
2024-08-07 14:32:23 -0600

    Add requirements.txt and finish two_segment_learning

--- experiments/two_segment_learning/two_segment_learning.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-import numpy as np
-
-training_epochs = [1024, 2048, 4096]
-model_sizes = [[32, 32], [64, 64], [32, 32, 32], [64, 64, 64], [128]]
-datapoints = [2**13, 2**14, 2**15, 2**16]
-
-test_size = 2**12
